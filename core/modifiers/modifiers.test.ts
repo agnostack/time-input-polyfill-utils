c021c2203e381ff753173977659ac80f96245594
Daniel Tonon <dan503.npm@gmail.com>
2019-12-12 21:56:04 +1100

    Pairing back the scope of modifiers objects
    I am usually either incrementing/decrementiong OR converting. I don't need to do both at the same time. It is overcomplicating the situation.

--- core/modifiers/modifiers.test.ts
+++ core/modifiers/modifiers.test.ts
@@ -1,61 +1,63 @@
-import { string12hr } from './modifiers'
+import { string12hr, string24hr, object12hr } from './modifiers'
 
 describe('Modify initial string values', () => {
 	//////////////////////////////////////////
 
-	describe('Increment hours to 12hr', () => {
+	describe('Increment 12hr hours', () => {
 		it('09:00 AM => 10:00 AM', () => {
-			expect(string12hr.increment.hrs.to12hr('09:00 AM')).to.equal(
-				'10:00 AM',
-			)
+			expect(string12hr.increment.hrs('09:00 AM')).to.equal('10:00 AM')
 		})
 		it('12:30 PM => 01:30 PM', () => {
-			expect(string12hr.increment.hrs.to12hr('12:30 PM')).to.equal(
-				'01:30 PM',
-			)
+			expect(string12hr.increment.hrs('12:30 PM')).to.equal('01:30 PM')
 		})
 		it('11:00 PM => 12:00 AM', () => {
-			expect(string12hr.increment.hrs.to12hr('11:00 PM')).to.equal(
-				'12:00 AM',
-			)
+			expect(string12hr.increment.hrs('11:00 PM')).to.equal('12:00 AM')
 		})
 	})
 
-	describe('Increment hours to 24hr', () => {
-		it('09:00 AM => 10:00', () => {
-			expect(string12hr.increment.hrs.to24hr('09:00 AM')).to.equal(
-				'10:00',
-			)
+	describe('Increment 24hr hours', () => {
+		it('09:00 => 10:00', () => {
+			expect(string24hr.increment.hrs('09:00')).to.equal('10:00')
 		})
-		it('12:30 PM => 13:30', () => {
-			expect(string12hr.increment.hrs.to24hr('12:30 PM')).to.equal(
-				'13:30',
-			)
+		it('12:30 => 13:30', () => {
+			expect(string24hr.increment.hrs('12:30')).to.equal('13:30')
 		})
-		it('11:00 PM => 00:00', () => {
-			expect(string12hr.increment.hrs.to24hr('11:00 PM')).to.equal(
-				'00:00',
-			)
+		it('23:00 => 00:00', () => {
+			expect(string24hr.increment.hrs('23:00')).to.equal('00:00')
 		})
 	})
 
-	describe('Increment hours to object', () => {
-		it('09:00 AM => {hrs:10, min:0, mode:AM}', () => {
-			expect(string12hr.increment.hrs.toObject('09:00 AM')).to.equal({
+	describe('Increment object hours', () => {
+		it('{hrs:9, min:0, mode:AM} => {hrs:10, min:0, mode:AM}', () => {
+			expect(
+				object12hr.increment.hrs({
+					hrs: 9,
+					min: 0,
+					mode: 'AM',
+				}),
+			).to.equal({
 				hrs: 10,
 				min: 0,
 				mode: 'AM',
 			})
 		})
-		it('12:30 PM => {hrs:1, min:30, mode:PM}', () => {
-			expect(string12hr.increment.hrs.toObject('12:30 PM')).to.equal({
+		it('{hrs:12, min:30, mode:PM} => {hrs:1, min:30, mode:PM}', () => {
+			expect(
+				object12hr.increment.hrs({
+					hrs: 12,
+					min: 30,
+					mode: 'PM',
+				}),
+			).to.equal({
 				hrs: 1,
 				min: 30,
 				mode: 'PM',
 			})
 		})
-		it('11:00 PM => {hrs:12, min:0, mode:AM}', () => {
-			expect(string12hr.increment.hrs.toObject('11:00 PM')).to.equal({
+		it('{hrs:11, min:0, mode:PM} => {hrs:12, min:0, mode:AM}', () => {
+			expect(
+				object12hr.increment.hrs({ hrs: 11, min: 0, mode: 'PM' }),
+			).to.equal({
 				hrs: 12,
 				min: 0,
 				mode: 'AM',
--- core/modifiers/modifiers.ts
+++ core/modifiers/modifiers.ts
@@ -2,42 +2,22 @@ import { TimeObject } from '../../types/timeObject'
 
 export const string12hr = {
 	increment: {
-		hrs: {
-			to12hr: (time12hr: string): string => '01:30 PM',
-			to24hr: (time12hr: string): string => '13:30',
-			toObject: (time12hr: string): TimeObject => ({
-				hrs: 1,
-				min: 30,
-				mode: 'PM',
-			}),
-		},
+		hrs: (time12hr: string): string => '01:30 PM',
 	},
 }
 
 export const string24hr = {
 	increment: {
-		hrs: {
-			to12hr: (time24hr: string): string => '01:30 PM',
-			to24hr: (time24hr: string): string => '13:30',
-			toObject: (time24hr: string): TimeObject => ({
-				hrs: 1,
-				min: 30,
-				mode: 'PM',
-			}),
-		},
+		hrs: (time24hr: string): string => '13:30',
 	},
 }
 
-export const timeObject = {
+export const object12hr = {
 	increment: {
-		hrs: {
-			to12hr: (timeObject: TimeObject): string => '01:30 PM',
-			to24hr: (timeObject: TimeObject): string => '13:30',
-			toObject: (timeObject: TimeObject): TimeObject => ({
-				hrs: 1,
-				min: 30,
-				mode: 'PM',
-			}),
-		},
+		hrs: (timeObject: TimeObject): TimeObject => ({
+			hrs: 1,
+			min: 30,
+			mode: 'PM',
+		}),
 	},
 }
