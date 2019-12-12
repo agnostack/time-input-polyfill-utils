b29de7c0406fa96a165b59125a933fa0ad80356e
Daniel Tonon <dan503.npm@gmail.com>
2019-12-12 22:54:31 +1100

    Updating converters tests to the new format

--- core/converters/converters.test.ts
+++ core/converters/converters.test.ts
@@ -1,23 +1,17 @@
-import {
-	convert_hours_to_12hr_time,
-	convert_number,
-	leading_zero,
-	convert_to_12hr_time,
-	convert_to_24hr_time,
-} from './converters'
+import { convert_number, leading_zero, convert } from './converters'
 
-describe('convert_hours_to_12hr_time', () => {
+describe('convert.hours24', () => {
 	it('Expect 0 hrs to be 12', () => {
-		expect(convert_hours_to_12hr_time(0)).to.equal(12)
+		expect(convert.hours24(0).toHours12()).to.equal(12)
 	})
 	it('Expect 5 hrs to be 5', () => {
-		expect(convert_hours_to_12hr_time(5)).to.equal(5)
+		expect(convert.hours24(5).toHours12()).to.equal(5)
 	})
 	it('Expect 13 hrs to be 1', () => {
-		expect(convert_hours_to_12hr_time(13)).to.equal(1)
+		expect(convert.hours24(13).toHours12()).to.equal(1)
 	})
 	it('Expect 24 hrs to be 0', () => {
-		expect(convert_hours_to_12hr_time(24)).to.equal(12)
+		expect(convert.hours24(24).toHours12()).to.equal(12)
 	})
 })
 
@@ -48,69 +42,69 @@ describe('leading_zero', () => {
 	})
 })
 
-describe('convert_to_12hr_time', () => {
+describe('convert string 24hr', () => {
 	it('Expect "" to be "--:-- --"', () => {
-		expect(convert_to_12hr_time('')).to.equal('--:-- --')
+		expect(convert.string24hr('').to12hr()).to.equal('--:-- --')
 	})
 	it('Expect "0:00" to be "12:00 AM"', () => {
-		expect(convert_to_12hr_time('0:00')).to.equal('12:00 AM')
+		expect(convert.string24hr('0:00').to12hr()).to.equal('12:00 AM')
 	})
 	it('Expect "5:30" to be "05:30 AM"', () => {
-		expect(convert_to_12hr_time('5:30')).to.equal('05:30 AM')
+		expect(convert.string24hr('5:30').to12hr()).to.equal('05:30 AM')
 	})
 	it('Expect "05:30" to be "05:30 AM"', () => {
-		expect(convert_to_12hr_time('5:30')).to.equal('05:30 AM')
+		expect(convert.string24hr('5:30').to12hr()).to.equal('05:30 AM')
 	})
 	it('Expect "11:00" to be "11:00 AM"', () => {
-		expect(convert_to_12hr_time('11:00')).to.equal('11:00 AM')
+		expect(convert.string24hr('11:00').to12hr()).to.equal('11:00 AM')
 	})
 	it('Expect "12:00" to be "12:00 PM"', () => {
-		expect(convert_to_12hr_time('12:00')).to.equal('12:00 PM')
+		expect(convert.string24hr('12:00').to12hr()).to.equal('12:00 PM')
 	})
 	it('Expect "13:00" to be "01:00 PM"', () => {
-		expect(convert_to_12hr_time('13:00')).to.equal('01:00 PM')
+		expect(convert.string24hr('13:00').to12hr()).to.equal('01:00 PM')
 	})
 	it('Expect "24:00" to be "12:00 AM"', () => {
-		expect(convert_to_12hr_time('24:00')).to.equal('12:00 AM')
+		expect(convert.string24hr('24:00').to12hr()).to.equal('12:00 AM')
 	})
 	it('Expect "25:30" to error', () => {
 		try {
-			convert_to_12hr_time('25:30')
+			convert.string24hr('25:30').to12hr()
 		} catch (error) {
 			expect(error.message).to.equal('Hours cannot be higher than 24')
 		}
 	})
 })
 
-describe('convert_to_24hr_time', () => {
+describe('convert string 12hr', () => {
 	it('Expect "--:-- --" to be ""', () => {
-		expect(convert_to_24hr_time('--:-- --')).to.equal('')
+		expect(convert.string12hr('--:-- --').to24hr()).to.equal('')
 	})
 	it('Expect "01:-- --" to be ""', () => {
-		expect(convert_to_24hr_time('01:-- --')).to.equal('')
+		expect(convert.string12hr('01:-- --').to24hr()).to.equal('')
 	})
 	it('Expect "--:02 --" to be ""', () => {
-		expect(convert_to_24hr_time('--:02 --')).to.equal('')
+		expect(convert.string12hr('--:02 --').to24hr()).to.equal('')
 	})
 	it('Expect "--:-- AM" to be ""', () => {
-		expect(convert_to_24hr_time('--:-- AM')).to.equal('')
+		expect(convert.string12hr('--:-- AM').to24hr()).to.equal('')
 	})
 	it('Expect "12:00 AM" to be "00:00"', () => {
-		expect(convert_to_24hr_time('12:00 AM')).to.equal('00:00')
+		expect(convert.string12hr('12:00 AM').to24hr()).to.equal('00:00')
 	})
 	it('Expect "05:30 AM" to be "05:30"', () => {
-		expect(convert_to_24hr_time('05:30 AM')).to.equal('05:30')
+		expect(convert.string12hr('05:30 AM').to24hr()).to.equal('05:30')
 	})
 	it('Expect "11:00 AM" to be "11:00"', () => {
-		expect(convert_to_24hr_time('11:00 AM')).to.equal('11:00')
+		expect(convert.string12hr('11:00 AM').to24hr()).to.equal('11:00')
 	})
 	it('Expect "12:00 PM" to be "12:00"', () => {
-		expect(convert_to_24hr_time('12:00 PM')).to.equal('12:00')
+		expect(convert.string12hr('12:00 PM').to24hr()).to.equal('12:00')
 	})
 	it('Expect "01:00 PM" to be "13:00"', () => {
-		expect(convert_to_24hr_time('01:00 PM')).to.equal('13:00')
+		expect(convert.string12hr('01:00 PM').to24hr()).to.equal('13:00')
 	})
 	it('Expect "11:30 PM" to be "23:30"', () => {
-		expect(convert_to_24hr_time('11:30 PM')).to.equal('23:30')
+		expect(convert.string12hr('11:30 PM').to24hr()).to.equal('23:30')
 	})
 })
--- core/converters/converters.ts
+++ core/converters/converters.ts
@@ -1,8 +1,5 @@
-import { Hour24 } from '../../types'
+import { Hour24, Hour12 } from '../../types'
 import { TimeObject } from '../../types/timeObject'
-
-export const convert_hours_to_12hr_time = (hours: Hour24) =>
-	hours <= 12 ? (hours === 0 ? 12 : hours) : hours - 12
 
 export const convert_number = (number: any) =>
 	isNaN(number) ? number : parseInt(number)
@@ -13,44 +10,27 @@ export const leading_zero = (number: any) => {
 	return purified < 10 ? `0${purified}` : `${purified}`
 }
 
-export const convert_to_12hr_time = (timeString_24hr: string) => {
-	if (timeString_24hr === '') return '--:-- --'
-	const twentyFour_regex = /([0-9]{1,2})\:([0-9]{2})/
-	const result = twentyFour_regex.exec(timeString_24hr)
-	const hrs_24 = convert_number(result[1])
-	if (hrs_24 > 24) {
-		throw new Error('Hours cannot be higher than 24')
-	}
-	const min = result[2]
-	const hrs_12 = convert_hours_to_12hr_time(hrs_24)
-	const isPM = 12 <= hrs_24 && hrs_24 < 24
-	const mode = isPM ? 'PM' : 'AM'
-	return [leading_zero(hrs_12), ':', min, ' ', mode].join('')
-}
-
 // Can't really test this one since it needs a node list as input which isn't possible in Node.js
 export const toArray = (NodeList: NodeList) =>
 	Array.prototype.slice.call(NodeList, 0)
 
-export const convert_to_24hr_time = (timeString_12hr: string) => {
-	if (/-/.test(timeString_12hr)) return ''
-	const isPM = timeString_12hr.indexOf('PM') > -1
-	const timeResult = /^([0-9]{2})/.exec(timeString_12hr)
-	const hrs = timeResult ? parseInt(timeResult[1]) : 0
-	let newHrs
-	if (hrs === 12) {
-		newHrs = isPM ? 12 : 0
-	} else {
-		newHrs = isPM ? hrs + 12 : hrs
-	}
-	const finalHrs = newHrs === 24 ? 0 : newHrs
-	const timeRegEx = /^[0-9]{2}:([0-9]{2}) (AM|PM)/
-	return timeString_12hr.replace(timeRegEx, leading_zero(finalHrs) + ':$1')
-}
-
 export const convert = {
 	string12hr: (string12hr: string) => ({
-		to24hr: (): string => '01:00',
+		to24hr: (): string => {
+			if (/-/.test(string12hr)) return ''
+			const isPM = string12hr.indexOf('PM') > -1
+			const timeResult = /^([0-9]{2})/.exec(string12hr)
+			const hrs = timeResult ? parseInt(timeResult[1]) : 0
+			let newHrs
+			if (hrs === 12) {
+				newHrs = isPM ? 12 : 0
+			} else {
+				newHrs = isPM ? hrs + 12 : hrs
+			}
+			const finalHrs = newHrs === 24 ? 0 : newHrs
+			const timeRegEx = /^[0-9]{2}:([0-9]{2}) (AM|PM)/
+			return string12hr.replace(timeRegEx, leading_zero(finalHrs) + ':$1')
+		},
 		toTimeObject: (): TimeObject => ({
 			hrs: 1,
 			min: 0,
@@ -58,7 +38,20 @@ export const convert = {
 		}),
 	}),
 	string24hr: (string24hr: string) => ({
-		to12hr: (): string => '01:00 AM',
+		to12hr: (): string => {
+			if (string24hr === '') return '--:-- --'
+			const twentyFour_regex = /([0-9]{1,2})\:([0-9]{2})/
+			const result = twentyFour_regex.exec(string24hr)
+			const hrs_24 = convert_number(result[1])
+			if (hrs_24 > 24) {
+				throw new Error('Hours cannot be higher than 24')
+			}
+			const min = result[2]
+			const hrs_12 = convert.hours24(hrs_24).toHours12()
+			const isPM = 12 <= hrs_24 && hrs_24 < 24
+			const mode = isPM ? 'PM' : 'AM'
+			return [leading_zero(hrs_12), ':', min, ' ', mode].join('')
+		},
 		toTimeObject: (): TimeObject => ({
 			hrs: 1,
 			min: 0,
@@ -68,5 +61,11 @@ export const convert = {
 	timeObject: (timeObject: TimeObject) => ({
 		to12hr: (): string => '01:00 AM',
 		to24hr: () => '01:00',
+	}),
+	hours24: (hours24: Hour24) => ({
+		toHours12: (): Hour12 =>
+			<Hour12>(
+				(hours24 <= 12 ? (hours24 === 0 ? 12 : hours24) : hours24 - 12)
+			),
 	}),
 }
