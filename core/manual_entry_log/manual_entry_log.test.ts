1b2811d411727ed8cc6d61983c283de73e69a640
Daniel Tonon <dan503.npm@gmail.com>
2019-12-12 18:59:23 +1100

    Switching completly over to Cypress as the testing platform
    Having two testing platforms is stupid and Cypress does more of what I need than Jest.

--- core/manual_entry_log/manual_entry_log.test.ts
+++ core/manual_entry_log/manual_entry_log.test.ts
@@ -2,71 +2,71 @@ import { manual_entry_log } from './manual_entry_log'
 
 const entryLog = new manual_entry_log()
 
-test('Expect hrs to be []', () => {
-	expect(entryLog.hrs).toStrictEqual([])
+it('Expect hrs to be []', () => {
+	expect(entryLog.hrs).to.deep.equal([])
 })
 
-test('Expect min to be []', () => {
-	expect(entryLog.min).toStrictEqual([])
+it('Expect min to be []', () => {
+	expect(entryLog.min).to.deep.equal([])
 })
 
-test('Expect mode to be []', () => {
-	expect(entryLog.mode).toStrictEqual([])
+it('Expect mode to be []', () => {
+	expect(entryLog.mode).to.deep.equal([])
 })
 
 // ==========================
 
-test('Adds "1" to hrs', () => {
+it('Adds "1" to hrs', () => {
 	entryLog.add('hrs', '1')
-	expect(entryLog.hrs).toStrictEqual([1])
+	expect(entryLog.hrs).to.deep.equal([1])
 })
-test('Adds "1" to min', () => {
+it('Adds "1" to min', () => {
 	entryLog.add('min', '1')
-	expect(entryLog.min).toStrictEqual([1])
+	expect(entryLog.min).to.deep.equal([1])
 })
-test('Adds "1" to mode', () => {
+it('Adds "1" to mode', () => {
 	entryLog.add('mode', '1')
-	expect(entryLog.mode).toStrictEqual([1])
+	expect(entryLog.mode).to.deep.equal([1])
 })
 
 // ==========================
 
-test('Adds "2" to hrs', () => {
+it('Adds "2" to hrs', () => {
 	entryLog.add('hrs', '2')
-	expect(entryLog.hrs).toStrictEqual([1, 2])
+	expect(entryLog.hrs).to.deep.equal([1, 2])
 })
-test('Adds "2" to min', () => {
+it('Adds "2" to min', () => {
 	entryLog.add('min', '2')
-	expect(entryLog.min).toStrictEqual([1, 2])
+	expect(entryLog.min).to.deep.equal([1, 2])
 })
-test('Adds "2" to mode', () => {
+it('Adds "2" to mode', () => {
 	entryLog.add('mode', '2')
-	expect(entryLog.mode).toStrictEqual([1, 2])
+	expect(entryLog.mode).to.deep.equal([1, 2])
 })
 
 // ==========================
 
-test('clears hrs', () => {
+it('clears hrs', () => {
 	entryLog.clear('hrs')
-	expect(entryLog.hrs).toStrictEqual([])
+	expect(entryLog.hrs).to.deep.equal([])
 })
-test('clears min', () => {
+it('clears min', () => {
 	entryLog.clear('min')
-	expect(entryLog.min).toStrictEqual([])
+	expect(entryLog.min).to.deep.equal([])
 })
-test('clears mode', () => {
+it('clears mode', () => {
 	entryLog.clear('mode')
-	expect(entryLog.mode).toStrictEqual([])
+	expect(entryLog.mode).to.deep.equal([])
 })
 
 // ==========================
 
-test('clear all', () => {
+it('clear all', () => {
 	entryLog.add('hrs', '1')
 	entryLog.add('min', '1')
 	entryLog.add('mode', '1')
 	entryLog.clearAll()
-	expect(entryLog.hrs).toStrictEqual([])
-	expect(entryLog.min).toStrictEqual([])
-	expect(entryLog.mode).toStrictEqual([])
+	expect(entryLog.hrs).to.deep.equal([])
+	expect(entryLog.min).to.deep.equal([])
+	expect(entryLog.mode).to.deep.equal([])
 })
