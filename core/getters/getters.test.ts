5dde5012a8d70aff45477005f53c11d82fc625b8
Daniel Tonon <dan503.npm@gmail.com>
2019-12-15 00:59:13 +1100

    updating the getters tests

--- core/getters/getters.test.ts
+++ core/getters/getters.test.ts
@@ -1,18 +1,31 @@
-import { get_values } from './getters'
+import { get } from './getters'
 
-describe('get_values', () => {
-	it('expects 12:30 PM => {hrs:12,min:30,mode:PM}', () => {
-		expect(get_values('12:30 PM')).to.deep.equal({
-			hrs: 12,
-			min: 30,
-			mode: 'PM',
-		})
+describe('get string 12hr', () => {
+	it('expects get 12:30 PM hrs12 => 12', () => {
+		expect(get.string12hr('12:30 PM').hrs12).to.equal(12)
 	})
-	it('expects 08:30 AM => {hrs:8,min:30,mode:AM}', () => {
-		expect(get_values('08:30 AM')).to.deep.equal({
-			hrs: 8,
-			min: 30,
-			mode: 'AM',
-		})
+	it('expects get 1:30 PM hrs24 => 13', () => {
+		expect(get.string12hr('1:30 PM').hrs24).to.equal(13)
+	})
+	it('expects get 1:30 PM min => 30', () => {
+		expect(get.string12hr('1:30 PM').min).to.equal(30)
+	})
+	it('expects get 1:30 PM mode => PM', () => {
+		expect(get.string12hr('1:30 PM').mode).to.equal('PM')
+	})
+})
+
+describe('get string 24hr', () => {
+	it('expects get 0:30 hrs12 => 12', () => {
+		expect(get.string24hr('0:30').hrs12).to.equal(12)
+	})
+	it('expects get 0:30 hrs24 => 0', () => {
+		expect(get.string24hr('0:30').hrs24).to.equal(0)
+	})
+	it('expects get 0:30 min => 30', () => {
+		expect(get.string24hr('0:30').min).to.equal(30)
+	})
+	it('expects get 0:30 mode => AM', () => {
+		expect(get.string24hr('0:30').mode).to.equal('AM')
 	})
 })
