03dfe9b4257f9ca82946aca7c8ac32a6a3aa5b5d
Daniel Tonon <dan503.npm@gmail.com>
2019-12-18 21:41:38 +1100

    Wrote a bunch of failing tests for the validate time object funcition

--- core/validate/validate.test.js
+++ core/validate/validate.test.js
@@ -3,6 +3,8 @@ import { failTest } from '../../cypress/helpers/failTest'
 
 /* global describe, expect, it */
 
+const writeBadValue = badValue => (typeof badValue === 'string' ? `"${badValue}"` : badValue)
+
 describe('validate 12 hour time', () => {
 	it('"12:00" FAIL', () => {
 		failTest(() => validate.string12hr('12:00'), '"12:00" is not a valid 12 hour time')
@@ -64,7 +66,129 @@ describe('validate 24 hour time', () => {
 })
 
 describe('validate time object', () => {
-	it('needs tests written', () => {
-		failTest(() => {}, '???')
+	it('basic time object', () => {
+		expect(validate.timeObject({ hrs24: 13, hrs12: 1, min: 0, mode: 'PM' })).to.equal(true)
+	})
+	it('hrs12 & hrs24 mismatch', () => {
+		failTest(
+			() => validate.timeObject({ hrs24: 1, hrs12: 1, min: 0, mode: 'PM' }),
+			'If mode is "PM", hrs24 should be 12 hours ahead of hrs12',
+		)
+	})
+	describe('invalid properties', () => {
+		it('"hrs" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hrs: 1, min: 0, mode: 'AM' }),
+				'the "hrs" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hrs": 1, "min": 0, "mode": "AM" }',
+			)
+		})
+		it('"hour" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hour: 1, min: 0, mode: 'AM' }),
+				'the "hour" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hour": 1, "min": 0, "mode": "AM" }',
+			)
+		})
+		it('"hours" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hours: 1, min: 0, mode: 'AM' }),
+				'the "hours" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hours": 1, "min": 0, "mode": "AM" }',
+			)
+		})
+		it('"hours12" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hours12: 1, min: 0, mode: 'AM' }),
+				'the "hours12" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hrs24": 1, "hours12": 1, "min": 0, "mode": "AM" }',
+			)
+		})
+		it('"hours24" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hours24: 1, min: 0, mode: 'AM' }),
+				'the "hours24" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hours24": 1, "hrs12": 1, "min": 0, "mode": "AM" }',
+			)
+		})
+		it('"mins" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hrs: 1, mins: 0, mode: 'AM' }),
+				'the "mins" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hrs24": 1, "hrs12": 1, "mins": 0, "mode": "AM" }',
+			)
+		})
+		it('"minutes" property is not valid', () => {
+			failTest(
+				() => validate.timeObject({ hrs24: 1, hrs12: 1, minutes: 0, mode: 'AM' }),
+				'the "minutes" property is not valid, valid properties are "hrs12", "hrs24", "min", and "mode": { "hrs24": 1, "hrs12": 1, "minutes": 0, "mode": "AM" }',
+			)
+		})
+	})
+	describe('invalid property types', () => {
+		const numberTypeCheck = ({ propName, lower, upper }) => {
+			const validTimeObject = { hrs24: 1, hrs12: 1, min: 0, mode: 'AM' }
+			const goodValue = validTimeObject[propName]
+			const badPropValues = [`${goodValue}`, goodValue + 1, goodValue - 1]
+			const badTimeObjects = badPropValues.map(badValue => ({
+				...validTimeObject,
+				[propName]: badValue,
+			}))
+
+			const testNames = [
+				`"${propName}" is an invalid string`,
+				`"${propName}" is out of upper range (${upper + 1})`,
+				`"${propName}" is out of lower range (${lower - 1})`,
+			]
+
+			describe(propName, () => {
+				testNames.forEach((testName, index) => {
+					const badValue = writeBadValue(badPropValues[index])
+					it(testName, () => {
+						failTest(
+							() => validate.timeObject(badTimeObjects[index]),
+							`"${propName}" must be a number ${lower}-${upper} or "--": ${propName} = ${badValue}`,
+						)
+					})
+				})
+			})
+		}
+
+		numberTypeCheck({ propName: 'hrs24', lower: 0, upper: 23 })
+		numberTypeCheck({ propName: 'hrs12', lower: 1, upper: 12 })
+		numberTypeCheck({ propName: 'min', lower: 0, upper: 59 })
+
+		describe('mode', () => {
+			const modeTest = (testName, mode) => {
+				const badValue = writeBadValue(mode)
+				it(testName, () => {
+					failTest(
+						() => validate.timeObject({ hrs24: 1, hrs12: 1, min: 0, mode }),
+						`Mode value is invalid, valid mode values are "AM", "PM", and "--": mode = ${badValue}`,
+					)
+				})
+			}
+			modeTest('empty string invalid', '')
+			modeTest('lower case is invalid', 'am')
+			modeTest('nonsense is invalid', 'abc')
+			modeTest('numbers are invalid', 1)
+		})
+	})
+
+	describe('missing properties', () => {
+		const validTimeObject = { hrs24: 1, hrs12: 1, min: 0, mode: 'AM' }
+
+		const missingPropTest = missingProp => {
+			const badTimeObject = { ...validTimeObject }
+			delete badTimeObject[missingProp]
+
+			it(`${missingProp} property is missing`, () => {
+				failTest(
+					() => validate.timeObject(badTimeObject),
+					`timeObject is missing the "${missingProp}" property: ${JSON.stringify(
+						badTimeObject,
+					)}`,
+				)
+			})
+		}
+
+		missingPropTest('hrs24')
+		missingPropTest('hrs12')
+		missingPropTest('min')
+		missingPropTest('mode')
 	})
 })
