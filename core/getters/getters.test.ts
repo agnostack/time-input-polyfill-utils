b3f081b659ab87419ca50985142da32648c67a36
Daniel Tonon <dan503.npm@gmail.com>
2019-12-22 20:19:30 +1100

    Fixing getters test

--- core/getters/getters.test.ts
+++ core/getters/getters.test.ts
@@ -17,15 +17,15 @@ describe('get string 12hr', () => {
 
 describe('get string 24hr', () => {
 	it('expects get 0:30 hrs12 => 12', () => {
-		expect(get.string24hr('0:30').hrs12).to.equal(12)
+		expect(get.string24hr('00:30').hrs12).to.equal(12)
 	})
 	it('expects get 0:30 hrs24 => 0', () => {
-		expect(get.string24hr('0:30').hrs24).to.equal(0)
+		expect(get.string24hr('00:30').hrs24).to.equal(0)
 	})
 	it('expects get 0:30 min => 30', () => {
-		expect(get.string24hr('0:30').min).to.equal(30)
+		expect(get.string24hr('00:30').min).to.equal(30)
 	})
 	it('expects get 0:30 mode => AM', () => {
-		expect(get.string24hr('0:30').mode).to.equal('AM')
+		expect(get.string24hr('00:30').mode).to.equal('AM')
 	})
 })
