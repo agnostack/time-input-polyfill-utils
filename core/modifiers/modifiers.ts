b4426f56670bc2c2a9e2055d556bca62df72df7a
Daniel Tonon <dan503.npm@gmail.com>
2019-12-26 18:34:42 +1100

    Decrement tests are working now

--- core/modifiers/modifiers.ts
+++ core/modifiers/modifiers.ts
@@ -148,15 +148,15 @@ export const modify = {
 					const copiedObject = { ...timeObject }
 
 					if (typeof hrs12 === 'number') {
-						if (hrs12 === 11) {
-							copiedObject.hrs12 = 12
+						if (hrs12 === 12) {
+							copiedObject.hrs12 = 11
 						} else {
-							if (hrs12 === maxAndMins.hrs12.max) {
-								copiedObject.hrs12 = maxAndMins.hrs12.min
+							if (hrs12 === maxAndMins.hrs12.min) {
+								copiedObject.hrs12 = maxAndMins.hrs12.max
 							}
 
-							if (hrs12 < maxAndMins.hrs12.max) {
-								copiedObject.hrs12 = <Hour12>(hrs12 + 1)
+							if (hrs12 > maxAndMins.hrs12.min) {
+								copiedObject.hrs12 = <Hour12>(hrs12 - 1)
 							}
 						}
 					} else {
@@ -165,7 +165,7 @@ export const modify = {
 					}
 
 					// This aligns the hrs24 value with the hrs12 value
-					const newString12hr = convert.timeObject(copiedObject).to12hr()
+					const newString12hr = convert.timeObject(copiedObject, true).to12hr()
 					return convert.string12hr(newString12hr).toTimeObject()
 				},
 				integrated: (): TimeObject => {
@@ -173,16 +173,16 @@ export const modify = {
 					const copiedObject = { ...timeObject }
 
 					if (typeof hrs24 === 'number') {
-						if (hrs24 === maxAndMins.hrs24.max) {
-							copiedObject.hrs24 = maxAndMins.hrs24.min
+						if (hrs24 === maxAndMins.hrs24.min) {
+							copiedObject.hrs24 = maxAndMins.hrs24.max
 						}
 
-						if (hrs24 < maxAndMins.hrs24.max) {
-							copiedObject.hrs24 = <Hour24>(hrs24 + 1)
+						if (hrs24 > maxAndMins.hrs24.min) {
+							copiedObject.hrs24 = <Hour24>(hrs24 - 1)
 						}
 
 						// This aligns the hrs12 value with the hrs24 value
-						const newString24hr = convert.timeObject(copiedObject).to24hr()
+						const newString24hr = convert.timeObject(copiedObject, true).to24hr()
 						return convert.string24hr(newString24hr).toTimeObject()
 					} else {
 						// If it isn't a number, then it is better to increment in isolation
