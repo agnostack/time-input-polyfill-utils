6ca8e15c37b08d9fbe8855548a61902d4ddc7cd9
Daniel Tonon <dan503.npm@gmail.com>
2019-12-27 21:20:30 +1100

    Updating getters to the new format

--- core/getters/getters.test.ts
+++ core/getters/getters.test.ts
@@ -1,31 +1,38 @@
 import { get } from './getters'
 
-describe('get string 12hr', () => {
-	it('expects get 12:30 PM hrs12 => 12', () => {
-		expect(get.string12hr('12:30 PM').hrs12).to.equal(12)
-	})
-	it('expects get 1:30 PM hrs24 => 13', () => {
-		expect(get.string12hr('1:30 PM').hrs24).to.equal(13)
-	})
-	it('expects get 1:30 PM min => 30', () => {
-		expect(get.string12hr('1:30 PM').min).to.equal(30)
-	})
-	it('expects get 1:30 PM mode => PM', () => {
-		expect(get.string12hr('1:30 PM').mode).to.equal('PM')
-	})
-})
+getString12hrTests()
+getString24hrTests()
 
-describe('get string 24hr', () => {
-	it('expects get 0:30 hrs12 => 12', () => {
-		expect(get.string24hr('00:30').hrs12).to.equal(12)
+function getString12hrTests() {
+	describe('get string 12hr', () => {
+		it('expects get 12:30 PM hrs12 => 12', () => {
+			expect(get.string12hr('12:30 PM').hrs12).to.equal(12)
+		})
+		it('expects get 1:30 PM hrs24 => 13', () => {
+			expect(get.string12hr('1:30 PM').hrs24).to.equal(13)
+		})
+		it('expects get 1:30 PM min => 30', () => {
+			expect(get.string12hr('1:30 PM').min).to.equal(30)
+		})
+		it('expects get 1:30 PM mode => PM', () => {
+			expect(get.string12hr('1:30 PM').mode).to.equal('PM')
+		})
 	})
-	it('expects get 0:30 hrs24 => 0', () => {
-		expect(get.string24hr('00:30').hrs24).to.equal(0)
+}
+
+function getString24hrTests() {
+	describe('get string 24hr', () => {
+		it('expects get 0:30 hrs12 => 12', () => {
+			expect(get.string24hr('00:30').hrs12).to.equal(12)
+		})
+		it('expects get 0:30 hrs24 => 0', () => {
+			expect(get.string24hr('00:30').hrs24).to.equal(0)
+		})
+		it('expects get 0:30 min => 30', () => {
+			expect(get.string24hr('00:30').min).to.equal(30)
+		})
+		it('expects get 0:30 mode => AM', () => {
+			expect(get.string24hr('00:30').mode).to.equal('AM')
+		})
 	})
-	it('expects get 0:30 min => 30', () => {
-		expect(get.string24hr('00:30').min).to.equal(30)
-	})
-	it('expects get 0:30 mode => AM', () => {
-		expect(get.string24hr('00:30').mode).to.equal('AM')
-	})
-})
+}
