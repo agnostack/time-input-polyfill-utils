4efa38a1a7d1f62f422c15fef457a6e16f34c965
Daniel Tonon <dan503.npm@gmail.com>
2020-02-16 23:05:53 +1100

    extending loadInputElement function to also provide access to window and document properties
    Needed for the a11y block to target the correct <body> element

--- core/select/select.test.ts
+++ core/select/select.test.ts
@@ -28,7 +28,7 @@ function testCursorRangeValues() {
 	describe('Test cursor range values', () => {
 		const testCursorRange = (index: number, expectation: SelectionRange) => {
 			it(`test range index ${index}`, async () => {
-				const $input = await loadInputElement()
+				const { $input } = await loadInputElement()
 				$input.selectionStart = index
 				const output = get.rangeOf($input).cursorSegment()
 				expect(output).to.deep.equal(expectation)
@@ -53,7 +53,7 @@ function testCursorSegmentSelection() {
 	describe('Test cursor segment selection', () => {
 		const testCursorSelection = (index: number, expectation: SelectionRange) => {
 			it(`select segment at index ${index}`, async () => {
-				const $input = await loadInputElement()
+				const { $input } = await loadInputElement()
 				$input.selectionStart = index
 				select($input).cursorSegment()
 				expectRange($input, expectation)
@@ -78,7 +78,7 @@ function testSpecificSegmentSelection() {
 	describe('Test specific segment selection', () => {
 		const testSegmentSelect = (segment: Segment, expectation: SelectionRange) => {
 			it(`select segment ${segment}`, async () => {
-				const $input = await loadInputElement()
+				const { $input } = await loadInputElement()
 				select($input).segment(segment)
 				expectRange($input, expectation)
 			})
@@ -94,7 +94,7 @@ function testNextSegmentSelection() {
 	describe('Test next segment selection', () => {
 		const testSegmentAfter = (segment: Segment, expectation: SelectionRange) => {
 			it(`select segment after ${segment}`, async () => {
-				const $input = await loadInputElement()
+				const { $input } = await loadInputElement()
 				select($input).segment(segment)
 				select($input).nextSegment()
 				expectRange($input, expectation)
@@ -111,7 +111,7 @@ function testPrevSegmentSelection() {
 	describe('Test previous segment selection', () => {
 		const testSegmentBefore = (segment: Segment, expectation: SelectionRange) => {
 			it(`select segment before ${segment}`, async () => {
-				const $input = await loadInputElement()
+				const { $input } = await loadInputElement()
 				select($input).segment(segment)
 				select($input).prevSegment()
 				expectRange($input, expectation)
--- cypress/support/loadInputElement.ts
+++ cypress/support/loadInputElement.ts
@@ -1,10 +1,16 @@
 import { inputID } from "./staticTestValues"
 
-export const loadInputElement = (): Promise<HTMLInputElement> => new Promise((resolve) => {
+interface LoadedPage {
+	$input: HTMLInputElement
+	document: Document
+	window: Window
+}
+
+export const loadInputElement = (): Promise<LoadedPage> => new Promise((resolve) => {
 	cy.visit('./cypress/test-file.html')
 		.then((contentWindow: Window) => {
 			let { document } = contentWindow
 			const $input = <HTMLInputElement>document.getElementById(inputID)
-			resolve($input)
+			resolve({ $input, document, window: contentWindow })
 		})
 })
