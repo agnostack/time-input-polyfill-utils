3bd90dc7f46c6ddb37d01453ec6677c6a046b118
Daniel Tonon <dan503.npm@gmail.com>
2020-02-17 23:38:30 +1100

    Creating a11y.update() function

--- core/a11y/a11y.ts
+++ core/a11y/a11y.ts
@@ -1,4 +1,7 @@
 import { a11yID } from "../../cypress/support/staticTestValues"
+import { get } from "../get/get"
+
+type Announcement = 'initial' | 'select' | 'update'
 
 export const a11y = {
 	create(document: Document = window.document): HTMLDivElement {
@@ -12,4 +15,40 @@ export const a11y = {
 		document.querySelector('body').appendChild($block)
 		return $block
 	},
+	update($input: HTMLInputElement, announcementArray: Array<Announcement>, document: Document = window.document) {
+		// Timeout helps ensure that the input has stabilized
+		setTimeout(function () {
+			const cursorSegment = get.rangeOf($input).cursorSegment().segment
+			const currentSegment = cursorSegment === 'hrs' ? 'hrs12' : cursorSegment
+
+			var values = get.inputValue($input).asTimeObject()
+			var value = values[currentSegment]
+			var segmentValue = value == '--' ? 'blank' : value
+
+			var segmentName = {
+				hrs12: 'Hours',
+				min: 'Minutes',
+				mode: 'AM/PM',
+			}[currentSegment]
+
+			var announcements = {
+				initial: '$label grouping $fullValue.',
+				select: '$segmentName spin button $segmentValue.',
+				update: '$segmentValue.',
+			}
+
+			var textArray = announcementArray.map((key: Announcement) => announcements[key])
+
+			var fullValue = $input.value.replace(/--/g, 'blank')
+
+			var html = `<p>${textArray.join('</p><p>')}</p>`
+			//TODO: create get.labelOf($input)
+			// html = html.replace(/\$label/g, $input.polyfill.label)
+			html = html.replace(/\$segmentName/g, segmentName)
+			html = html.replace(/\$segmentValue/g, `${segmentValue}`)
+			html = html.replace(/\$fullValue/g, fullValue)
+
+			document.getElementById(a11yID).innerHTML = html
+		})
+	}
 }
