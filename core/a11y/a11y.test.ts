54feafefad90df5c3bd1e75813e2e9bb28321729
Daniel Tonon <dan503.npm@gmail.com>
2020-03-15 17:05:38 +1100

    a11y.test.ts file update

--- core/a11y/a11y.test.ts
+++ core/a11y/a11y.test.ts
@@ -10,18 +10,30 @@ import { a11y } from "./a11y"
 	- a11y.update($input, [ announcementArray ], document)
 */
 
+async function createA11y() {
+	const { document, $input } = await loadTestPage()
+	a11y.create(document)
+	return {
+		$a11y: document.getElementById(a11yID),
+		$input,
+		document,
+	}
+}
 
-describe('Basic setup', () => {
-	it('Input exists', async () => {
-		await loadTestPage()
-		cy.get(`#${inputID}`).should('exist')
-	})
-})
+const getA11y = () => cy.get(`#${a11yID}`)
 
 describe('Create a11y element', () => {
 	it('a11y exists', async () => {
-		const { document } = await loadTestPage()
-		a11y.create(document)
-		cy.get(`#${a11yID}`).should('exist')
+		await createA11y()
+		getA11y().should('exist')
+	})
+})
+
+// TO DO: This test is failing
+describe('Update a11y element', () => {
+	it('a11y update [initial]', async () => {
+		const { $input, document } = await createA11y()
+		a11y.update($input, ['initial'], document)
+		getA11y().should('have.text', '???')
 	})
 })
