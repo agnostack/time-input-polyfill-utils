b19ac12c992ceec85ed5c8b6fff6a374aeff18fe
Daniel Tonon <dan503.npm@gmail.com>
2020-03-16 21:27:39 +1100

    adding "to do" in manual_entry_log.ts
    I'm a bit worried about if mode can be a number when it never is.
    
    I feel like this can probably be improved. I'll revisit after I've tried implementing this library in something.

--- core/manual_entry_log/manual_entry_log.ts
+++ core/manual_entry_log/manual_entry_log.ts
@@ -3,6 +3,8 @@ import { Segment } from '../../types'
 export class manual_entry_log {
 	hrs12: Array<number>
 	min: Array<number>
+	// TO DO: potential bugs can spawn from this.
+	// Revisit after implementing to see if this can be improved
 	mode: Array<number>
 
 	constructor() {
