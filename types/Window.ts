65611b36033d1ac246e20a0ef32852f8676c3b49
Daniel Tonon <dan503.npm@gmail.com>
2020-04-02 20:02:38 +1100

    Started applying the new types to the custom window object

--- types/Window.ts
+++ types/Window.ts
@@ -1,7 +1,55 @@
+import { A11yCreate, A11yUpdate } from '../core/a11y/a11y.types'
+import {
+	ToArray,
+	ToNumber,
+	ToLeadingZero,
+	ConvertString24hr,
+	ConvertTimeObject,
+	ConvertHours24,
+	ConvertDateObject,
+	ConvertString12hr,
+} from '../core/convert/convert.types'
+import { ManualEntryLog } from '../core/ManualEntryLog/ManualEntryLog'
+import { ModifyString12hr, ModifyString24hr, ModifyTimeObject } from '../core/modify/modify.types'
+import { QuerySelectAll } from '../core/select/select.types'
+
 declare global {
 	interface Window {
-		// TO DO: URGENT! need to figure out how to apply all the types information to this
-		timeInputPolyfillUtils: any
+		timeInputPolyfillUtils: {
+			a11yCreate: A11yCreate
+			a11yUpdate: A11yUpdate
+
+			toArray: ToArray
+			toNumber: ToNumber
+			toLeadingZero: ToLeadingZero
+
+			convertString12hr: ConvertString12hr
+			convertString24hr: ConvertString24hr
+			convertTimeObject: ConvertTimeObject
+			convertHours24: ConvertHours24
+			convertDateObject: ConvertDateObject
+
+			// TO DO: get
+
+			// TO DO: is
+
+			ManualEntryLog: typeof ManualEntryLog
+
+			modifyString12hr: ModifyString12hr
+			modifyString24hr: ModifyString24hr
+			modifyTimeObject: ModifyTimeObject
+
+			regex: {
+				string12hr: RegExp
+				string24hr: RegExp
+			}
+
+			_$$: QuerySelectAll
+
+			// TO DO: select
+
+			// TO DO: validate
+		}
 		supportsTime?: boolean
 	}
 }
