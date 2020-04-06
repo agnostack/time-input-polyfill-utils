192d7bd3d73c25ed3d709d91762f069acad5ab98
Daniel Tonon <dan503.npm@gmail.com>
2020-04-06 22:22:50 +1000

    Putting a safe-guard in so that loadJS() does not download any code twice

--- common/loadJS.ts
+++ common/loadJS.ts
@@ -1,6 +1,13 @@
+import '../types/Window'
+
+const alreadyCalledSources: Array<string> = []
+
 export default function loadJS(src: string, callback: Function): void {
-	const script = document.createElement('script')
-	script.src = src
-	script.onload = (): any => callback()
-	document.head.appendChild(script)
+	if (alreadyCalledSources.indexOf(src) < 0) {
+		alreadyCalledSources.push(src)
+		const script = document.createElement('script')
+		script.src = src
+		script.onload = (): any => callback()
+		document.head.appendChild(script)
+	}
 }
