dcfa25f2556444792599d23ab6d076594536353c
Daniel Tonon <dan503.npm@gmail.com>
2020-05-02 01:05:30 +1000

    2nd attempt at fixing loadJS not calling all callback funcitons

--- common/loadJS.ts
+++ common/loadJS.ts
@@ -1,15 +1,36 @@
 import '../types/Window'
 
 const alreadyCalledSources: Array<string> = []
+const loadedSources: Array<string> = []
+
+interface Callbacks {
+	[key: string]: Array<Function>
+}
+
+const awaitingCallbacks: Callbacks = {}
+
+const addCallback = (src: string, callback: Function): void => {
+	if (awaitingCallbacks[src]) {
+		awaitingCallbacks[src].push(callback)
+	} else {
+		awaitingCallbacks[src] = [callback]
+	}
+}
 
 export default function loadJS(src: string, callback: Function): void {
 	if (alreadyCalledSources.indexOf(src) < 0) {
 		alreadyCalledSources.push(src)
 		const script = document.createElement('script')
 		script.src = src
-		script.onload = (): any => callback()
+		script.onload = (): any => {
+			addCallback(src, callback)
+
+			for (const key in awaitingCallbacks) {
+				awaitingCallbacks[key].forEach(cb => cb())
+			}
+		}
 		document.head.appendChild(script)
 	} else {
-		callback()
+		addCallback(src, callback)
 	}
 }
