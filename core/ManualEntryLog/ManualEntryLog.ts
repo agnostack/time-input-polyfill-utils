e2ad0959b4ca10f2cf555eede9d2ba067a4ad822
Daniel Tonon <dan503.npm@gmail.com>
2020-08-15 17:22:01 +1000

    Refactoring ManualEntryLog to handle mode through the SegmentLog

--- core/ManualEntryLog/ManualEntryLog.ts
+++ core/ManualEntryLog/ManualEntryLog.ts
@@ -1,23 +1,35 @@
-import { Segment, Hour12, TimeObject, Minute, Mode, Hrs12, minutes } from '../../types/index'
+import { Segment, Hour12, TimeObject, Minute, Mode } from '../../types/index'
 import { maxAndMins } from '../staticValues'
 
 class SegmentLog {
-	value: Hour12 | Minute
-	segment: Hrs12 | minutes
-	entries: Array<number> = []
+	value: Hour12 | Minute | Mode
+	segment: Segment
+	entries: Array<number | string> = []
 
-	constructor(startingValue: Hour12 | Minute, segment: Hrs12 | minutes) {
+	constructor(startingValue: Hour12 | Minute | Mode, segment: Segment) {
 		this.value = startingValue
 		this.segment = segment
 	}
 
 	/**
 	 * Adds a value to the to the log and keeps track of what the end value should be
-	 * @param keyName - Expected to be a keyboard key name like "1" or "4"
+	 * @param keyName - Expected to be a keyboard key name like "1" or "a"
 	 */
 	add(keyName: string): void {
 		const number = parseInt(keyName)
-		if (!isNaN(number) && !(this.entries.length === 0 && number === 0)) {
+		const isZero = this.entries.length === 0 && number === 0
+
+		// Handles AM/PM
+		if (this.segment === 'mode') {
+			if (keyName.toLowerCase() === 'a') {
+				this.value = 'AM'
+				this.entries = [keyName]
+			}
+			if (keyName.toLowerCase() === 'p') {
+				this.value = 'PM'
+				this.entries = [keyName]
+			}
+		} else if (!isNaN(number) && !isZero) {
 			if ([0, 2].includes(this.entries.length)) {
 				this.entries = [number]
 			} else {
@@ -54,58 +66,20 @@ class SegmentLog {
 }
 
 class SegmentLogHrs extends SegmentLog {
-	value: Hour12
 	constructor(startingValue: Hour12) {
 		super(startingValue, 'hrs12')
-		this.value = startingValue
 	}
 }
+
 class SegmentLogMin extends SegmentLog {
-	value: Minute
 	constructor(startingValue: Minute) {
 		super(startingValue, 'minutes')
-		this.value = startingValue
 	}
 }
 
-class SegmentLogMode {
-	value: Mode
-	entries: Array<string> = []
-
+class SegmentLogMode extends SegmentLog {
 	constructor(startingValue: Mode) {
-		this.value = startingValue
-	}
-
-	/**
-	 * Adds a value to the to the log and keeps track of what the end value should be
-	 * @param keyName - Expected to be a keyboard key name like "a" or "p"
-	 */
-	add(keyName: string): void {
-		if (keyName.toLowerCase() === 'a') {
-			this.value = 'AM'
-			this.entries = [keyName]
-		}
-		if (keyName.toLowerCase() === 'p') {
-			this.value = 'PM'
-			this.entries = [keyName]
-		}
-	}
-
-	/**
-	 * Reset is needed for things like typing "1", then leaving, then coming back.
-	 *
-	 * The tracker should reset if they are returning.
-	 */
-	reset(): void {
-		this.entries = []
-	}
-
-	/**
-	 * Deletes the current value. Use this if the user presses delete or backspace.
-	 */
-	clear(): void {
-		this.reset()
-		this.value = '--'
+		super(startingValue, 'mode')
 	}
 }
 
@@ -126,39 +100,11 @@ export class ManualEntryLog {
 	}
 
 	/**
-	 * Reset is needed for things like typing "1", then leaving, then coming back.
-	 *
-	 * The tracker should reset if they are returning.
-	 *
-	 * @param segment - The segment that needs resetting
-	 */
-	reset(segment: Segment): void {
-		this[segment].reset()
-	}
-
-	/**
-	 * Deletes the current value. Use this if the user presses delete or backspace.
-	 * @param segment - The segment that needs to be cleared.
-	 */
-	clear(segment: Segment): void {
-		this[segment].clear()
-	}
-
-	/**
 	 * Deletes all of the values for all of the segments.
 	 */
 	clearAll(): void {
 		this.hrs12.clear()
 		this.minutes.clear()
 		this.mode.clear()
-	}
-
-	/**
-	 * Adds a value to the to the log and keeps track of what the end value should be
-	 * @param segment - The segment being added to
-	 * @param keyName - Expected to be a keyboard key name like "1" or "a"
-	 */
-	add(segment: Segment, keyName: string): void {
-		this[segment].add(keyName)
 	}
 }
