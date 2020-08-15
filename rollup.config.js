9b881cc80fb72052a6b9415540f431edf004599f
Daniel Tonon <dan503.npm@gmail.com>
2020-08-15 18:06:02 +1000

    Improving the publish to npm funcitonality
    It compiles everything into a single "npm" folder making cleanup after compilation much simpler

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ var typeScript = require('@rollup/plugin-typescript')
 var babel = require('rollup-plugin-babel')
 var { terser } = require('rollup-plugin-terser')
 
+var tsConfig = require('./tsconfig.prepublish.json')
+
 export default {
 	input: 'timeInputPolyfillUtils.ts',
 	plugins: [typeScript(), babel(), terser()],
 	output: {
-		file: 'time-input-polyfill-utils.min.js',
+		file: `${tsConfig.compilerOptions.outDir}/time-input-polyfill-utils.min.js`,
 		format: 'iife',
 	},
 }
