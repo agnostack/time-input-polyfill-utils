514dd7210a9fa5f979f1c1785d63eafb89cfabd4
Daniel Tonon <dan503.npm@gmail.com>
2020-08-15 14:37:14 +1000

    ManualEntryLog progress

--- core/ManualEntryLog/ManualEntryLog.ts
+++ core/ManualEntryLog/ManualEntryLog.ts
@@ -1,18 +1,25 @@
-import { Segment, Hour12, TimeObject } from '../../types/index'
+import { Segment, Hour12, TimeObject, Minute, Mode, Hrs12, Min } from '../../types/index'
 
-class SegmentLogHrs {
-	value: Hour12
+class SegmentLog {
+	value: Hour12 | Minute
+	segment: Hrs12 | Min
 	entries: Array<number> = []
 	isFull = true
 
-	constructor(startingValue: Hour12) {
+	constructor(startingValue: Hour12 | Minute, segment: Hrs12 | Min) {
 		this.value = startingValue
+		this.segment = segment
 	}
 
 	add(value: string): void {
 		const number = parseInt(value)
 		if (!isNaN(number)) {
-			if (number > 1) {
+			const value_x10_is_greater_than_max = {
+				hrs12: number > 1,
+				min: number > 5,
+			}[this.segment]
+
+			if (value_x10_is_greater_than_max) {
 				this.entries = [0, number]
 			} else {
 				if (this.entries.length === 0) {
@@ -25,12 +32,65 @@ class SegmentLogHrs {
 					this.entries = [number]
 				}
 			}
+
+			this.value = <Hour12 | Minute>parseInt(this.entries.join(''))
 		}
 	}
 
 	reset(): void {
 		this.entries = []
 		this.isFull = true
+	}
+
+	clear(): void {
+		this.reset()
+		this.value = '--'
+	}
+}
+
+class SegmentLogHrs extends SegmentLog {
+	value: Hour12
+	constructor(startingValue: Hour12) {
+		super(startingValue, 'hrs12')
+		this.value = startingValue
+	}
+}
+class SegmentLogMin extends SegmentLog {
+	value: Minute
+	constructor(startingValue: Minute) {
+		super(startingValue, 'min')
+		this.value = startingValue
+	}
+}
+
+class SegmentLogMode {
+	value: Mode
+	entries: Array<string> = []
+	isFull = true
+
+	constructor(startingValue: Mode) {
+		this.value = startingValue
+	}
+
+	add(value: string): void {
+		if (value.toLowerCase() === 'a') {
+			this.value = 'AM'
+			this.entries = [value]
+		}
+		if (value.toLowerCase() === 'p') {
+			this.value = 'PM'
+			this.entries = [value]
+		}
+	}
+
+	reset(): void {
+		this.entries = []
+		this.isFull = true
+	}
+
+	clear(): void {
+		this.reset()
+		this.value = '--'
 	}
 }
 
@@ -47,16 +107,16 @@ export class ManualEntryLog {
 	}
 
 	clear(segment: Segment): void {
-		this[segment] = []
+		this[segment].reset()
 	}
 
 	clearAll(): void {
 		this.hrs12.clear()
-		this.min = []
-		this.mode = []
+		this.min.clear()
+		this.mode.clear()
 	}
 
-	add(segment: Segment, entry: number | string): void {
+	add(segment: Segment, entry: string): void {
 		this[segment].add(entry)
 	}
 }
