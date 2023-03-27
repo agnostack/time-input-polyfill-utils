86dead02e6fa4e36b6e77dd87362496f3c1fd9c8
Adam Grohs <adam@uniquelyparticular.com>
2023-03-27 11:34:35 -0400

    feat: moved to umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,53 @@
-var typeScript = require('@rollup/plugin-typescript')
-var babel = require('rollup-plugin-babel')
-var { terser } = require('rollup-plugin-terser')
+const packageInfo = require('./package.json')
+const filesize = require('rollup-plugin-filesize')
 
-var tsConfig = require('./tsconfig.prepublish.json')
+const typeScript = require('@rollup/plugin-typescript')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const { babel } = require('@rollup/plugin-babel')
+const { terser } = require('rollup-plugin-terser')
+
+const _dependencies = Object.keys(packageInfo.dependencies || {})
+const _peerDependencies = Object.keys(packageInfo.peerDependencies || {})
+const _bundledDependencies = packageInfo.bundledDependencies || packageInfo.bundleDependencies || []
+const extenalDependencies = [..._dependencies, ..._peerDependencies].filter((extenalDependency) => (
+  !_bundledDependencies.includes(extenalDependency)
+))
+
+const globals = {
+  ..._dependencies.reduce((_globals, _dependency) => ({
+    ..._globals,
+    [_dependency]: _dependency,
+  }), {}),
+  ..._peerDependencies.reduce((_globals, _dependency) => ({
+    ..._globals,
+    [_dependency]: _dependency,
+  }), {}),
+}
 
 export default {
 	input: 'timeInputPolyfillUtils.ts',
-	plugins: [typeScript(), babel(), terser({ output: { comments: false } })],
-	output: {
-		file: `${tsConfig.compilerOptions.outDir}/time-input-polyfill-utils.min.js`,
-		format: 'iife',
-	},
+  external: extenalDependencies,
+	plugins: [
+    nodeResolve({ browser: true }),
+		typeScript(),
+		babel(),
+		filesize(),
+	],
+	output: [
+		...packageInfo.browser ? [{
+      format: 'umd',
+			globals,
+      name: packageInfo.name,
+      file: packageInfo.browser,
+      inlineDynamicImports: true,
+		}] : [],
+		...packageInfo.jsdelivr ? [{
+      format: 'umd',
+			globals,
+      name: packageInfo.name,
+      file: packageInfo.jsdelivr,
+      inlineDynamicImports: true,
+			plugins: [terser({ output: { comments: /@preserve|@license|@copyright|@cc_on/i } })]
+		}] : []
+	],
 }
